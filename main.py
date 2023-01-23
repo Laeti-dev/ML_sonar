095e3955b7fd00831189c639e0b3a662fe24fa67
Laetitia Taddei <laetdi@gmail.com>
2023-01-23 07:13:06 +0100

    add columns names to df

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 
-observations = pd.read_csv('./datas/sonar.all-data.csv')
+# observations = pd.read_csv('./datas/sonar.all-data.csv')
+# Decouvrir le DF
+# print(observations.head())
+# print(observations.columns.values)
+# print(observations.shape)
+# add columns name
+names = [f'F{x}' for x in range(1,61)]
+names.append('OBJET')
+print(names)
 
+observations = pd.read_csv('./datas/sonar.all-data.csv', names=names)
 print(observations.head())
-print(observations.columns.values)
-print(observations.shape)
